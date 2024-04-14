e6e218a45d724ddd4549139dd5fa675cb389c39e
leohsieh <bardiche4768@gmail.com>
2024-04-14 16:13:55 -0400

    minor change

--- scan_filter/main.cpp
+++ scan_filter/main.cpp
@@ -2,12 +2,14 @@
 #include <scan_filter/scan_filter.h>
 
 
-int main(int argc, char **argv) {
+int main (int argc, char **argv)
+{
     FLAGS_colorlogtostderr = true;
     google::InstallFailureSignalHandler();
 
-    ros::init(argc, argv, ros::this_node::getName());
-    ros::NodeHandle nh(ros::this_node::getName()); 
+    const std::string & name = ros::this_node::getName();
+    ros::init(argc, argv, name);
+    ros::NodeHandle nh(name); 
 
     beyond_nerf::ScanFilter filter(nh);
     ros::Subscriber sub = nh.subscribe("raw_scan", 1000, 
