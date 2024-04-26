cb4392e8324365b2013b0888c9b0ff28e89dcdea
leohsieh <bardiche4768@gmail.com>
2024-04-26 14:19:47 -0400

    published xyzi

--- dust3r/dust3r_server.py
+++ dust3r/dust3r_server.py
@@ -5,6 +5,9 @@ import numpy as np
 import rospy
 
 from bnerf_msgs.srv import LoadDUSt3R, LoadDUSt3RRequest, LoadDUSt3RResponse
+from sensor_msgs import point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
 
 import glob
 from datetime import datetime
@@ -16,9 +19,9 @@ class OfflineDUSt3RServer:
         self.data = {}
 
         def line_to_nsecs(line:str):
-                secs, nsecs = line.split('.')
-                secs = datetime.strptime(secs, "%Y-%m-%d %H:%M:%S")
-                return int(secs.timestamp() * 1e9) + int(nsecs)
+            secs, nsecs = line.split('.')
+            secs = datetime.strptime(secs, "%Y-%m-%d %H:%M:%S")
+            return int(secs.timestamp() * 1e9) + int(nsecs)
         
         for idx in ["02", "03"]:
             cam_dir = join(data_dir, "image_" + idx)
@@ -38,6 +41,31 @@ class OfflineDUSt3RServer:
             #npy_files = glob.glob(os.path.join(xyz_conf, "*.npy"))
             #self.data["cam%s" % cam_idx] = "image_%s" % cam_idx
 
+        self.pubs = {}
+        for frame_id in self.data:
+            topic = "dust3r/" + frame_id
+            pub = rospy.Publisher(topic, PointCloud2, queue_size=10)
+            self.pubs[frame_id] = pub
+
+
+    def publish_pointcloud(self, frame_id, views):
+        views = [np.load(view).reshape((-1, 4)) for view in views]
+
+        header = Header()
+        header.frame_id = frame_id
+        header.stamp = rospy.Time.now()
+
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                  PointField('y', 4, PointField.FLOAT32, 1),
+                  PointField('z', 8, PointField.FLOAT32, 1),
+                  PointField('intensity', 12, PointField.FLOAT32, 1)]
+        msg = pc2.create_cloud(header, fields, np.vstack(views))
+
+        # view1, view2 = np.load(view1), np.load(view2)
+        self.pubs[frame_id].publish(msg)
+
+
+
     def service_callback(self, req: LoadDUSt3RRequest):
         res = LoadDUSt3RResponse()
 
@@ -53,7 +81,6 @@ class OfflineDUSt3RServer:
             return res
         
         idx1, idx2 = data["idx"][t1], data["idx"][t2]
-        print(idx1, idx2)
         get_npy = lambda i: "%010d_%010d_view%d.npy" % (idx1, idx2, i)
         files = [join(data["xyz_conf"], get_npy(i)) for i in (1, 2)]
 
@@ -62,9 +89,8 @@ class OfflineDUSt3RServer:
             return res
         
         res.status = LoadDUSt3RResponse.SUCCESS
-        clouds = []
-        for file in files:
-            pass
+        if req.publish:
+            self.publish_pointcloud(frame_id, files)
 
         return res
 
