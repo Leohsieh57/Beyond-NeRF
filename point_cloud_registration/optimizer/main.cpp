7eb546d82762c3e234c585ee95bf72c90c95579d
leohsieh <bardiche4768@gmail.com>
2024-05-02 15:08:46 -0400

    se3 optimizer okay

--- point_cloud_registration/optimizer/main.cpp
+++ point_cloud_registration/optimizer/main.cpp
@@ -3,6 +3,19 @@
 
 
 using namespace bnerf;
+unique_ptr<Optimizer> optimizer;
+
+void ScanCallBack(const CloudXYZ::ConstPtr & cloud)
+{
+    const auto last_target = optimizer->GetInputTarget();
+    optimizer->SetInputTarget(cloud);
+
+    if (!last_target)
+        return;
+
+    optimizer->SetInputSource(last_target);
+    optimizer->OptimizeAlignment(SE3d());
+}
 
 
 int main(int argc, char **argv) {
@@ -10,29 +23,11 @@ int main(int argc, char **argv) {
     google::InstallFailureSignalHandler();
 
     ros::init(argc, argv, ros::this_node::getName());
-    ros::NodeHandle nh(ros::this_node::getName());
+    ros::NodeHandle nh("~");
 
-    Optimizer solver;
-    auto loader = ScanLoader::CreatePtr();
+    optimizer.reset(new Optimizer);
+    auto sub = nh.subscribe("input_scan", 100, ScanCallBack);
     
-    vector<ScanLoader::Regist> regs;
-    for (const auto &seq : loader->GetSeqs()) {
-        loader->LookUpRegists(seq, regs);
-        
-        for (const auto &[map_id, scan_ids] : regs) {
-            auto cloud = loader->Load<Scan::RAW_SCAN>(seq, map_id);
-            solver.SetInputTarget(cloud);
-            for (const int &scan_id : scan_ids) {
-                if (!ros::ok())
-                    return 0;
-
-                auto cloud = loader->Load<Scan::FILTERED>(seq, scan_id);
-                solver.SetInputSource(cloud);
-                solver.OptimizeAlignment(SE3d());
-            }
-        } 
-    }
-
     ros::spin();
     return 0;
 }
